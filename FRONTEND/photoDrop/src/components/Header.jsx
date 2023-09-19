311503fb885f3d199d17ab6c7a54c374e88ead1a
Carlos Gascon <crlsgscnls@gmail.com>
2023-09-19 18:46:03 +0200

    proyecto photodrop

--- FRONTEND/photoDrop/src/components/Header.jsx
+++ FRONTEND/photoDrop/src/components/Header.jsx
@@ -1,7 +1,30 @@
 import "../styles/Header.css"
+import { Link } from "react-router-dom";
 
-export function Header(){
-    return <header>
-        <h1>PhotoDrop</h1>
-    </header>
+const isActiveRoute = (path) => {
+    return location.pathname === path;
+};
+
+const showLink = (path) => {
+    if (location.pathname === "/register") {
+        return path === "/";
+    } else if (location.pathname === "/validate-email") {
+        return path === "/";
+    } else {
+        return !isActiveRoute("/login") || path === "/";
+    }
+};
+
+export function Header() {
+    return (
+        <header>
+            {showLink("/") && (
+                <Link to="/">
+                    <h1>PhotoDrop</h1>
+                </Link>
+            )}
+        </header>
+    )
 }
+
+
